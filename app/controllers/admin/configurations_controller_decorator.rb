a7f2b4b71bebe20df6bfda94bd1cfeed08620a6a
Chris Boertien <chris.boertien@gmail.com>
2011-11-22 14:31:40 -0700

    Move controller override to its own decorator
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

---
+++ app/controllers/admin/configurations_controller_decorator.rb
@@ -0,0 +1,14 @@
+module Admin
+  class ConfigurationsController.class_eval do
+    before_filter :add_flexi_rate_links, :only => :index
+
+    def add_flexi_rate_links
+      @extension_links << {
+        :link => admin_flexi_shipping_rates_path,
+        :link_text => FlexiShippingRate.human_name(:count => 2),
+        :description => t('flexi_shipping_rates_description')
+      }
+    end
+  end
+end
+
