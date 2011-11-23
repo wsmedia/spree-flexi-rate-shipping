93720ab3edb4cef44b639e548b55a86518670b1d
Chris Boertien <chris.boertien@gmail.com>
2011-11-22 17:41:16 -0700

    update migration to the newer change method
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- db/migrate/20081020154802_create_flexi_shipping_rates.rb
+++ db/migrate/20081020154802_create_flexi_shipping_rates.rb
@@ -1,5 +1,5 @@
 class CreateFlexiShippingRates < ActiveRecord::Migration
-  def self.up
+  def change
     create_table :flexi_shipping_rates do |t|
       t.references :shipping_category
       t.references :zone
@@ -10,8 +10,5 @@ class CreateFlexiShippingRates < ActiveRecord::Migration
       t.timestamps
     end
   end
-
-  def self.down
-    drop_table :flexi_shipping_rates
-  end
 end
+
